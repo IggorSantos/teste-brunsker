66033627e1ad0504e681059c7dbeac802ca0d540
Igor Santos <iggor.jsantos@gmail.com>
2023-03-26 10:02:19 -0300

    feat: criação da pagina de imovel

---
+++ src/app/pages/imovel-page/imovel-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImovelPageComponent } from './imovel-page.component';
+
+describe('ImovelPageComponent', () => {
+  let component: ImovelPageComponent;
+  let fixture: ComponentFixture<ImovelPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImovelPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ImovelPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
